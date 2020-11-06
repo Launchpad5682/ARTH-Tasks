232a54927d39a6c80157bb5068714f539a29264e
Rahul H Bhatia <rahul_harikishan@srmuniv.edu.in>
2020-11-06 23:46:33 +0530

    Docker and Web Server Changes Added

--- task-8/main.py
+++ task-8/main.py
@@ -1,14 +1,13 @@
-from partition import partition
+# from partition import partition
 from hadoop import hadoop_setup
-
+from script import hello
 def main():
 
     print('Select the options to perform')
     print('1. AWS CLI')
     print('2. Hadoop')
     print('3. Modifying Storages')
-    print('4. Docker')
-    print('5. Web Server')
+    print('4. Docker and Web Server')
 
     main_var = int(input('Enter the option: '))
 
@@ -20,11 +19,12 @@ def main():
         elif main_var == 2:
             hadoop_setup.hadoop()
 
-        elif main_var == 3:
-            partition.partition()
+        # elif main_var == 3:
+        #     partition.partition()
 
         elif main_var == 4:
-            pass
+            hello()
+            
 
         else:
             break
