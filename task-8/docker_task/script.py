4c7a7779823af2d001f4b32a0a633ffbdcf36e19
Rahul H Bhatia <rahul_harikishan@srmuniv.edu.in>
2020-11-07 01:30:19 +0530

    AWS and Refactoring done

--- task-8/docker_task/script.py
+++ task-8/docker_task/script.py
@@ -27,14 +27,16 @@ def hello():
             envi = input("Where do you want to run this python interpreter? container/local\n")
             if(envi == "container"):
                 os.system("clear")
+                cd=os.getcwd()
                 os_name = input("Enter the name of the container ")
-                os.chdir("docker_python_task")
+                os.chdir("docker_task/docker_python_task")
                 os.system("docker build -t python_repl:v1 .")
                 print("Now you can proceed ahead with the docker container set up")
                 print(" \n \n Your Container Is setup , you can start it with Python3")
                 os.system(f"docker container run -it --name {os_name} python_repl:v1")
                 os.system(f"docker container stop {os_name}")
                 os.system(f"docker system prune --force")
+                os.chdir(cd)
             elif(envi == "local"):
                 os.system(f"yum -y install python3")
                 os.system(f"python3")
@@ -43,7 +45,7 @@ def hello():
             if(envi == "container"):
                 os.system("clear")
                 os_name = input("Enter the name of the container ")
-                os.chdir("web_server_task")
+                os.chdir("docker_task/web_server_task")
                 print("Now you can proceed ahead with the docker container set up")
                 os.system("docker build -t myrepl:v1 .")
                 print(" \n \n Your Container Is setup , you can start the web Server with /usr/sbin/httpd")
@@ -54,6 +56,10 @@ def hello():
                 os.system("yum -y install httpd")
                 os.system(f"systemctl start httpd")
         if(option == 3):
-            return
+            break
         if(option == 4):
             exit()
+        else:
+            input("Invalid Value, Press Enter to Continue")
+            continue
+
--- task-8/main.py
+++ task-8/main.py
@@ -1,33 +1,45 @@
 # from partition import partition
+import os
 from hadoop import hadoop_setup
-from script import hello
-def main():
+from docker_task import script
+from AWS import awsmain
 
+def main():
+    os.system("clear")
     print('Select the options to perform')
     print('1. AWS CLI')
     print('2. Hadoop')
     print('3. Modifying Storages')
     print('4. Docker and Web Server')
+    print('5. Quit')
 
     main_var = int(input('Enter the option: '))
 
     while True:
 
         if main_var == 1:
-            pass
+            awsmain.hello_from_aws()
+            continue
 
         elif main_var == 2:
             hadoop_setup.hadoop()
+            continue
 
         # elif main_var == 3:
         #     partition.partition()
 
         elif main_var == 4:
-            hello()
+            script.hello()
+            continue
             
+        elif main_var==5:
+            exit()
 
         else:
-            break
+            input("Invalid Value, Press Enter to Continue")
+            continue
+
+        continue
 
 
 main()
